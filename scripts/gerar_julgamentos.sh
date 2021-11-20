655321b18a9d2aef096d94a65def8b4d696976fd
Gabriel Fontes <eu@misterio.me>
2021-11-19 22:36:35 -0300

    atualiza nome das instancias judiciais

--- scripts/gerar_julgamentos.sh
+++ scripts/gerar_julgamentos.sh
@@ -3,7 +3,7 @@ processos="$(psql -c "COPY (SELECT id FROM processo) TO STDOUT WITH CSV")"
 
 for i in {1..300}; do
     processo="'$(shuf -n 1 <<< "$processos")'"
-    instancia="$(shuf -n 1 -e "'Corte Estadual'" "'Suprema Corte Específica'" "'Supremo Tribunal Federal'")"
+    instancia="$(shuf -n 1 -e "'Primeira Instância'" "'Segunda Instância'" "'Tribunal Superior'")"
     data="'$(shuf -n 1 -i 1980-2021)-$(shuf -n 1 -i 01-12)-$(shuf -n 1 -i 01-28)'"
     procedente="'$(shuf -n 1 -i 0-1)'"
 
